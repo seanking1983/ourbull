1f92d6cff921d2a5a0c10e8ce02f3fb23799180a
Xun Xu <xunx@dhcp-168-105-240-133.wireless.manoa.hawaii.edu>
2011-12-25 13:51:10 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ourbull::Application.config.secret_token = '70e4a2914b9d020b93d1794f95cb448e637761ac6155b7737a962517d9c8deef63e5ad871aa9b6544592f2278eb8d2d282a9b8ace4199b56e8958fac3a252649'
