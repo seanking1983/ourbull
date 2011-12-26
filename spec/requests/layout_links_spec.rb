eea80d2e5046a48a01ab3e4ceef85d4d470a6b63
Xun Xu <seanking1983@gmail.com>
2011-12-25 20:10:41 -1000

    17. changed one header link, + integration tests for header and footer links

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -32,8 +32,28 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
 
+# footer links
+
+  it "should have working links on footer" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About Us")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact Us")
+  end
 
 
+# header links
+
+  it "should have working links on header" do
+    visit root_path
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Sign Up"
+    response.should have_selector('title', :content => "Sign Up")    
+  end
 
 
 
