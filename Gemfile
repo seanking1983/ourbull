c24de6725babcf66d88640f4cdc0464d074e3ace
Xun Xu <seanking1983@gmail.com>
2011-12-25 20:21:32 -1000

    18. annotated user model and + debug info on application layout

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'spork'
 group :development do
   gem 'rspec-rails', '2.6.1'
   gem 'faker'
-  gem 'annotate', '2.4.0'
+  gem 'annotate', :git => 'git://github.com/ctran/annotate_models.git'
 end
 
 group :test do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,3 +2,14 @@ require 'spec_helper'
 
 describe User do
 end
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
