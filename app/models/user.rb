6cd4690a7ccfa2616b6347b6d969e4d12d0d3166
Xun Xu <seanking1983@gmail.com>
2011-12-25 20:36:31 -1000

    20. + validation for name, email presence and email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   
   
   attr_accessible :name, :email
+  
+  validates :name,    :presence   => true
+  validates :email,   :presence   => true,
+                      :uniqueness => { :case_sensitive => false }
 
 
 
