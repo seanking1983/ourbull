83019dec853c1e9d33709fa566f67e1e08893a12
Xun Xu <seanking1983@gmail.com>
2011-12-25 21:00:45 -1000

    21. + validation test for name, email presence, email uniqueness and up to case uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,56 @@
 require 'spec_helper'
 
 describe User do
+  
+  
+  before(:each) do
+    @attr = { :name => "Example User", 
+              :email => "user@abc.edu" }
+  end
+  
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+  
+  
+  # name
+  
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+  
+  
+  # email
+  
+  it "should require a email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "should reject duplicate email" do
+    User.create!(@attr)
+    duplicate_email_user = User.new(@attr)
+    duplicate_email_user.should_not be_valid
+  end
+  
+  it "should reject email identical up to case" do
+    User.create!(@attr)
+    upcased_email = @attr[:email].upcase
+    case_duplicate_email_user = User.new(@attr.merge(:email => upcased_email))
+    case_duplicate_email_user.should_not be_valid
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
 # == Schema Information
 #
