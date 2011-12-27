f3cea2510509263c17fc72a03c39bf39eabf8ada
Xun Xu <seanking1983@gmail.com>
2011-12-26 21:07:03 -1000

    + code for create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ApplicationController
   
   def new
     @title = "Sign Up"
+    @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    
+    if @user.save
+      flash[:success] = "Welcome to OurBull!"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
 end
